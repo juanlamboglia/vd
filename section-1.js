4d885bbc52d8c9c56099c85e05b2a11326214021
juanlamboglia <juanlamboglia3@gmail.com>
2024-07-10 23:28:37 -0500

    Add files via upload

--- section-1.js
+++ section-1.js
@@ -151,73 +151,6 @@ function loadData() {
     }
 }
 
-// Función para el autocompletar de ciudades
-function autocomplete(inp, arr) {
-    var currentFocus;
-    inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        this.parentNode.appendChild(a);
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                b = document.createElement("DIV");
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                b.addEventListener("click", function(e) {
-                    inp.value = this.getElementsByTagName("input")[0].value;
-                    closeAllLists();
-                });
-                a.appendChild(b);
-            }
-        }
-    });
-    inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-            currentFocus++;
-            addActive(x);
-        } else if (e.keyCode == 38) {
-            currentFocus--;
-            addActive(x);
-        } else if (e.keyCode == 13) {
-            e.preventDefault();
-            if (currentFocus > -1) {
-                if (x) x[currentFocus].click();
-            }
-        }
-    });
-    function addActive(x) {
-        if (!x) return false;
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-        for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
-        }
-    }
-    function closeAllLists(elmnt) {
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != inp) {
-                x[i].parentNode.removeChild(x[i]);
-            }
-        }
-    }
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
-}
-
 // Agrega un evento para cambiar la ocupación y actualizar la interfaz en consecuencia
 document.getElementById('ocupacion').addEventListener('change', function() {
     const ocupacion = document.getElementById('ocupacion').value;
@@ -256,11 +189,5 @@ document.getElementById('ocupacion').addEventListener('change', function() {
 document.getElementById('actividad-economica').addEventListener('change', function() {
     const actividadIndependiente = document.getElementById('actividad-economica').value;
     // Aquí puedes agregar lógica adicional si es necesario para `actividad-economica`
-});
-
-// Inicializa el autocompletar para las ciudades
-document.addEventListener('DOMContentLoaded', function() {
-    const cities = ["Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena", "Cúcuta", "Bucaramanga", "Pereira", "Manizales", "Ibagué"];
-    autocomplete(document.getElementById('ciudad-residencia'), cities);
 });
 
